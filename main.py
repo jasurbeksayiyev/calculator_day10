0db6d3bd70836c2a8f7d3791ad3c29843197a6d5
jasurbeksayiyev <147401777+jasurbeksayiyev@users.noreply.github.com>
2024-11-25 23:26:59 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+from random import choice
+
+import art
+print(art.logo)
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+     "+": add,
+     "-": subtract,
+     "*": multiply,
+     "/": divide
+}
+def calculator():
+    should_accumulate = True
+    num1 = float(input("enter a first number: "))
+    while should_accumulate:
+
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("enter a second number: "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f" {num1}  {operation_symbol} {num2} = {answer}")
+
+        hoice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to stop calculating. ")
+
+        if choice == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
+calculator()
+
+
+
